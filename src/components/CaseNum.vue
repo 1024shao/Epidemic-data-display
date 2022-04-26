6daa447f670f7ab01ef39cfce4acc19395c1fef2
1024shao <3059680677@qq.com>
2022-04-26 14:47:34 +0800

    test

--- src/components/CaseNum.vue
+++ src/components/CaseNum.vue
@@ -3,8 +3,8 @@
 		<div class="container">
 			<div class="title">
 				<span
-					>截至 {{ formatData(caseNumData.modifyTime) }} 全国数据统计 hhh sss
-					sssh</span
+					>截至 {{ formatData(caseNumData.modifyTime) }} 全国数据统计 hh s sh
+					sss sssh</span
 				>
 			</div>
 		</div>
